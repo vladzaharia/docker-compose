1ea6e998def9a4267e489bc40c9f924107451f72
Vlad Zaharia <hey@vlad.gg>
2023-04-29 01:48:24 +0000

    Add traefik

--- _agebox/git/setup_git_hooks.sh
+++ _agebox/git/setup_git_hooks.sh
@@ -3,9 +3,11 @@
 BASEDIR=$(dirname "$0")
 
 cp -f $BASEDIR/pre-commit $BASEDIR/../../.git/hooks/pre-commit
+cp -f $BASEDIR/pre-push $BASEDIR/../../.git/hooks/pre-push
 cp -f $BASEDIR/post-checkout-merge $BASEDIR/../../.git/hooks/post-checkout
 cp -f $BASEDIR/post-checkout-merge $BASEDIR/../../.git/hooks/post-merge
 
 chmod +x $BASEDIR/../../.git/hooks/pre-commit
+chmod +x $BASEDIR/../../.git/hooks/pre-push
 chmod +x $BASEDIR/../../.git/hooks/post-checkout
 chmod +x $BASEDIR/../../.git/hooks/post-merge
